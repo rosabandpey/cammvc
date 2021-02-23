4b73f3198257d2cac611fa395fa03e144240b9a8
rosabandpey <mahnazbandpey@gmail.com>
2021-02-23 23:53:13 +0330

    Handling  Exception in register method of api,Implementing error page

--- src/main/java/com/camp/cammvc/exception/ApiException.java
+++ src/main/java/com/camp/cammvc/exception/ApiException.java
@@ -2,8 +2,16 @@ package com.camp.cammvc.exception;
 
 public class ApiException extends RuntimeException {
 
+    public ApiException(Throwable cause) {
+        super(cause);
+    }
+
+    public ApiException(String message, Throwable cause) {
+        super(message, cause);
+    }
 
     public ApiException(String message) {
         super(message);
+
     }
 }
