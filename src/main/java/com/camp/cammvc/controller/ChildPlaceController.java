4ad7ac5b8b977adb2488530e0ddf2b9ab8905fb7
rosabandpey <mahnazbandpey@gmail.com>
2021-04-28 20:56:21 +0430

    Implementing ChildPlace and Place CONTROLLER ,Service and view  layer

--- src/main/java/com/camp/cammvc/controller/ChildPlaceController.java
+++ src/main/java/com/camp/cammvc/controller/ChildPlaceController.java
@@ -23,36 +23,25 @@ public class ChildPlaceController {
     @Autowired
     private PlaceService placeService;
 
-    Place place;
+   // Place place;
    // String placeName;
     //String username;
 
 
-    @RequestMapping(path={"/savePlace"},method = {RequestMethod.POST,RequestMethod.GET}, produces = {MediaType.APPLICATION_JSON_VALUE})
-    public String register(Model model ,ChildPlace childPlace,Model model2, AppUser appUser ) {
+    @RequestMapping(path={"/savePlace/{placeid}"},method = {RequestMethod.POST,RequestMethod.GET}, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public String register(Model model,@ModelAttribute ("place") Place place ,ChildPlace childPlace,Model model2, AppUser appUser,@RequestParam("placeid")String id ) {
        // placeName=place.getPlaceName();
        // System.out.println("placeName"+placeName);
         appUser.setId(3);
         childPlace.setUserChildPlace(appUser);
-        //place.setId(id);
-        //System.out.println(place.getId());
+        place.setId(Long.valueOf(id));
+        System.out.println(place.getId());
         childPlace.setMychildplace(place);
         childPlaceService.registerChildPlace(childPlace) ;
         return "redirect:/edit";
     }
 
 
-    @RequestMapping(path={"/setPlace/{id}"},method = {RequestMethod.POST,RequestMethod.GET}, produces = {MediaType.APPLICATION_JSON_VALUE})
-    public String setPlace(Model model, @PathVariable("id")long id ) {
-        // placeName=place.getPlaceName();
-        // System.out.println("placeName"+placeName);
-
-        place.setId(id);
-        System.out.println(place.getId());
-       // childPlace.setMychildplace(place);
-       // childPlaceService.registerChildPlace(childPlace) ;
-        return "redirect:/edit";
-    }
 
     @RequestMapping(path = {"/edit", "/edit/{id}"},method = {RequestMethod.GET,RequestMethod.POST})
     public String editUserById(Model model, ChildPlace childPlace)
